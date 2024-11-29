c71d30f8dd71949deec317a5e92112d04493a793
Daniel <humbertodanielcaballeros@gmail.com>
2024-11-29 11:26:34 -0500

    obtenerPelicula

--- obtenerPeliculas.js
+++ obtenerPeliculas.js
@@ -1,21 +1,40 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-const PELICULA_TABLE = process.env.TABLE_NAME_PELICULA;
 
 exports.handler = async () => {
+    console.log('Obteniendo todas las películas');
+
     try {
-        const params = { TableName: PELICULA_TABLE };
+        // Validar las variables de entorno
+        if (!process.env.TABLE_NAME_PELICULA) {
+            return {
+                statusCode: 500,
+                status: 'Internal Server Error - Variable de entorno TABLE_NAME_PELICULA no configurada',
+            };
+        }
+
+        const tabla_peliculas = process.env.TABLE_NAME_PELICULA;
+
+        // Configurar los parámetros para el escaneo de DynamoDB
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+        const params = { TableName: tabla_peliculas };
+
+        // Obtener todas las películas desde DynamoDB
         const result = await dynamodb.scan(params).promise();
 
+        // Respuesta exitosa
         return {
             statusCode: 200,
-            body: JSON.stringify({ peliculas: result.Items }),
+            message: 'Películas obtenidas exitosamente',
+            peliculas: result.Items,
         };
     } catch (error) {
+        console.error(`Error inesperado: ${error.message}`);
+
+        // Respuesta en caso de error
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Error al obtener las películas' }),
+            status: 'Internal Server Error - Error al obtener las películas',
+            error: error.message,
         };
     }
 };
